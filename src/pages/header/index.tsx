b840c06a47e0a7efdf33161d80d82cfeff9ea365
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-12-10 21:39:24 -0300

    menu e home com responsivos iniciais feitos

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -2,21 +2,44 @@ import Image from 'next/image';
 
 export const Header = () => {
   return (
-    <header className="w-full h-20 bg-transparent flex items-center justify-between absolute  border-b-2 border-opacity-10 border-white">
-      <div className="ml-6">
+    <header className="w-full h-20 md:h-24 bg-transparent flex items-center justify-between absolute border-b-2 border-opacity-10 border-white">
+      <div className="ml-6 md:ml-11 grid place-self-end">
         <Image
           src="/assets/logo-blizzard.png"
           alt="logo"
           width={69}
           height={32}
+          className="mb-6 md:mb-8 md:w-[87px]"
+        />
+        <Image
+          src="/assets/Vector 7.svg"
+          alt="detalhe-logo"
+          width={43}
+          height={1}
+          className="-mb-2"
         />
       </div>
-      <div className="mr-6">
+      <div className="mr-6 flex">
+        <div className="hidden md:flex mr-20 gap-4">
+          <a className="w-32 h-[41px] px-4 py-2.5 rounded-[3px] border border-white gap-[7px] text-center text-white text-sm font-medium cursor-pointer">
+            Criar Conta
+          </a>
+          <a className="w-[115px] h-[41px] px-[25px] py-2.5 bg-sky-500 rounded-[3.15px] gap-[6px] inline-flex text-center text-white text-sm font-medium cursor-pointer">
+            <Image
+              src="/assets/login.svg"
+              alt="detalhe menu"
+              width={18}
+              height={19}
+            />
+            Logar
+          </a>
+        </div>
         <Image
           src="/assets/menu.svg"
           alt="detalhe menu"
           width={28}
           height={19}
+          className=" xl:hidden"
         />
       </div>
     </header>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,20 +5,24 @@ import { ButtonComponents } from '../../components/button/index';
 
 export const HomePage: React.FC = () => {
   return (
-    <section>
-      <div className="w-full url h-[658px] bg-cover flex bg-center flex-col justify-between">
-        <div className="flex flex-col ml-6 self-baseline mt-32 gap-5">
-          <h1 className="w-[283px] text-white text-[40px] font-bold leading-[44px] md:w-[562px] md:text-[64px] md:leading-[70px]">
-            Retorne à escuridão com o game Diablo IV
-          </h1>
-          <p className="w-[283px] md:w-[550px] text-white text-lg font-normal">
-            O retorno de Lilith traz uma era de escuridão e sofrimento
-          </p>
+    <section className="w-full url h-[658px] bg-cover grid bg-center md:h-[758px] md:grid-cols-3 md:items-center">
+      <div className="ml-6 md:ml-12 col-span-2 xl:grid xl:grid-flow-col xl:grid-cols-2">
+        <div className="xl:-ml-36">
+          <div className="mt-32 md:mt-12 w-[283px] md:w-[562px]">
+            <h1 className="text-white text-[40px] font-bold leading-[44px] md:text-[64px] md:leading-[70px] md:whitespace-nowrap">
+              Retorne à <br /> escuridão com o{' '}
+              <br className="hidden md:block" />
+              game Diablo IV
+            </h1>
+            <p className="text-white text-lg font-normal mt-2">
+              O retorno de Lilith traz uma era de escuridão e sofrimento
+            </p>
+          </div>
+          <div className="mt-8">
+            <ButtonComponents text="Jogue Agora" type="button" />
+          </div>
         </div>
-        <div className="ml-6">
-          <ButtonComponents text="Jogue Agora" type="button" />
-        </div>
-        <div className="ml-6 flex mb-14 gap-4">
+        <div className="w-80 grid grid-flow-col xl:grid-flow-row mt-14 xl:col-start-1 xl:ml-28">
           <Image
             src="/assets/banner-hero/icons/game-1.png"
             alt="logo"
@@ -48,6 +52,25 @@ export const HomePage: React.FC = () => {
             alt="logo"
             width={48}
             height={48}
+          />
+        </div>
+      </div>
+      <div className="hidden md:grid gap-60 xl:gap-32 mr-12 mt-9 justify-center">
+        <Image
+          src="/assets/banner-hero/games/diablo-logo.png"
+          alt="logo"
+          width={280}
+          height={154}
+        />
+        <div className="grid justify-center">
+          <span className="text-right text-white text-[13px] font-semibold uppercase hidden md:block mb-4">
+            Assista o trailer
+          </span>
+          <Image
+            src="/assets/banner-hero/games/diablo-animation-cover.png"
+            alt="logo"
+            width={280}
+            height={158}
           />
         </div>
       </div>
